38a7bf3484f950193aa55580481636e01ad14558
divyam gupta <divyam7091@gmail.com>
2025-02-14 15:02:16 +0530

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
+export const metadata: Metadata = {
+  title: "UCHAI Assginment",
+  description: "Created by divyam",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
