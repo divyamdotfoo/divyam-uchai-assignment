0d9480378639971e8f379629b2b91fc688c52551
divyam gupta <divyam7091@gmail.com>
2025-02-17 08:21:11 +0530

    feat: add crud for cols and tasks

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -20,6 +20,19 @@ import { Task } from "./task";
 import { Plus } from "lucide-react";
 import { TaskView } from "./task-view";
 import { BoardSkeleton } from "./board-skeleton";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { generateContainerColor } from "@/lib/utils";
+import { nanoid } from "nanoid";
 
 export function Board() {
   const columns = useBoardStore.use.columns();
@@ -71,14 +84,59 @@ export function Board() {
           <Column column={col} key={col.id} />
         ))}
         <div className="h-full rounded-md p-4 w-[320px] shrink-0 bg-black flex items-center justify-center">
-          <div className=" w-20 h-20 rounded-full bg-card flex items-center justify-center group">
-            <Plus className=" w-16 h-16 group-hover:scale-105 transition-all cursor-pointer" />
-          </div>
+          <AddColumn />
         </div>
       </div>
 
       <DragOverlay>{activeId ? <Task taskId={activeId} /> : null}</DragOverlay>
       <TaskView />
     </DndContext>
+  );
+}
+
+function AddColumn() {
+  const [colName, setColName] = useState("");
+  const addCol = useBoardStore.use.addCol();
+  const handleClick = () => {
+    if (!colName) return;
+    addCol({
+      title: colName,
+      colors: generateContainerColor(),
+      id: nanoid(10),
+      taskIds: [],
+    });
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className=" w-20 h-20 rounded-full bg-card flex items-center justify-center group">
+          <Plus className=" w-16 h-16 group-hover:scale-105 transition-all cursor-pointer" />
+        </button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Column</DialogTitle>
+          <DialogDescription>
+            This will add a new column in the current board.
+          </DialogDescription>
+        </DialogHeader>
+        <div className=" w-full">
+          <Label>Column name</Label>
+          <Input
+            value={colName}
+            onChange={(e) => setColName(e.target.value)}
+            placeholder="Please enter column name"
+          />
+        </div>
+        <DialogClose asChild>
+          <button
+            onClick={handleClick}
+            className=" w-full bg-green-700 hover:bg-green-600 transition-all rounded-lg py-2 px-4 h-10 my-4"
+          >
+            Add Column
+          </button>
+        </DialogClose>
+      </DialogContent>
+    </Dialog>
   );
 }
--- src/components/column.tsx
+++ src/components/column.tsx
@@ -1,11 +1,35 @@
-import { type Column } from "@/store/types";
 import { useDroppable } from "@dnd-kit/core";
-import { Task } from "./task";
 import {
   SortableContext,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { Ellipsis, Plus } from "lucide-react";
+import { Ellipsis, Plus, Trash2 } from "lucide-react";
+import { nanoid } from "nanoid";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import type { Column, Priority, Task as TaskType, User } from "@/store/types";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { useBoardStore } from "@/store";
+import Image from "next/image";
+import { useState } from "react";
+import { Task } from "./task";
+import { formattedDate } from "@/lib/utils";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export function Column({ column }: { column: Column }) {
   const { setNodeRef } = useDroppable({ id: column.id });
@@ -53,12 +77,8 @@ export function Column({ column }: { column: Column }) {
                   </p>
                 </div>
                 <div className="flex items-center gap-3">
-                  <button className="w-7 h-7 flex items-center justify-center hover:bg-card rounded-md transition-all">
-                    <Plus size={20} />
-                  </button>
-                  <button className="w-7 h-7 flex items-center justify-center hover:bg-card rounded-md transition-all">
-                    <Ellipsis size={20} />
-                  </button>
+                  <AddTask column={column} />
+                  <DeleteThisCol id={column.id} />
                 </div>
               </div>
 
@@ -70,5 +90,141 @@ export function Column({ column }: { column: Column }) {
         </div>
       </div>
     </SortableContext>
+  );
+}
+
+function AddTask({ column }: { column: Column }) {
+  const users = useBoardStore.use.users();
+  const addTask = useBoardStore.use.addTask();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  const [assignee, setAssignee] = useState("");
+
+  const handleCreateTask = () => {
+    if (!title || !description || !priority || !assignee) {
+      return;
+    }
+
+    const newTask: TaskType = {
+      title,
+      description,
+      priority: priority as Priority,
+      assignee: users.find((user) => user.name === assignee) as User,
+      columnId: column.id,
+      attachments: [],
+      comments: [],
+      createdAt: formattedDate(),
+      id: nanoid(10),
+    };
+
+    addTask(newTask);
+    // Reset inputs after task creation
+    setTitle("");
+    setDescription("");
+    setPriority("");
+    setAssignee("");
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="w-7 h-7 flex items-center justify-center hover:bg-card rounded-md transition-all">
+          <Plus size={20} />
+        </button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Tender Task</DialogTitle>
+          <DialogDescription>
+            Your task will be added to the{" "}
+            <span className="font-semibold">{column.title}</span> column.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col w-full gap-4">
+          <div className="w-full">
+            <Label>Task Title</Label>
+            <Input
+              placeholder="Enter task title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="w-full">
+            <Label>Task Description</Label>
+            <Input
+              placeholder="Enter task description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between gap-5 w-full pt-4">
+            <Select value={priority} onValueChange={setPriority}>
+              <SelectTrigger className="basis-1/2 px-10">
+                <SelectValue placeholder="Choose priority" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="low">Low</SelectItem>
+                <SelectItem value="medium">Medium</SelectItem>
+                <SelectItem value="high">High</SelectItem>
+              </SelectContent>
+            </Select>
+            <Select value={assignee} onValueChange={setAssignee}>
+              <SelectTrigger className="basis-1/2 px-10">
+                <SelectValue placeholder="Choose Assignee" />
+              </SelectTrigger>
+              <SelectContent>
+                {users.map((user) => (
+                  <SelectItem key={user.name} value={user.name}>
+                    <div className="w-full flex items-center gap-3">
+                      <div className="w-5 h-5 rounded-full overflow-hidden">
+                        <Image
+                          src={user.avatar}
+                          alt="avatar"
+                          width={100}
+                          height={100}
+                          className="w-full h-full object-cover rounded-full"
+                        />
+                      </div>
+                      <p>{user.name}</p>
+                    </div>
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <DialogClose asChild>
+            <button
+              onClick={handleCreateTask}
+              className="bg-green-700 text-white h-10 py-2 px-4 font-medium text-center rounded-lg my-4"
+            >
+              Create Task
+            </button>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function DeleteThisCol({ id }: { id: string }) {
+  const delCol = useBoardStore.use.deleteCol();
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <button className="w-7 h-7 flex items-center justify-center hover:bg-card rounded-md transition-all">
+          <Ellipsis size={20} />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent className=" w-fit bg-card p-0">
+        <button
+          onClick={() => delCol(id)}
+          className=" flex items-center gap-2 bg-inherit hover:bg-black transition-all py-4 px-8 rounded-md"
+        >
+          <Trash2 size={20} />
+          Delete
+        </button>
+      </PopoverContent>
+    </Popover>
   );
 }
